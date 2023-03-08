6df022ed7d6ee85ea8a3e051dee3b9128cf06bdb
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2023-03-08 06:51:24 +0000

    update cloudformation configuration (#20)
    Co-authored-by: JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>

--- cmd/command/cloudformation_output.go
+++ cmd/command/cloudformation_output.go
@@ -410,6 +410,13 @@ type ssmKeyOpts struct {
 	ProviderName string
 }
 
+type ssmKeyOpts struct {
+	HandlerID    string
+	Key          string
+	Publisher    string
+	ProviderName string
+}
+
 // this will create a unique identifier for AWS System Manager Parameter Store
 // for configuration field "api_url" this will result: 'publisher/provider-name/version/configuration/api_url'
 func ssmKey(opts ssmKeyOpts) string {
